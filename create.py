b2e5b9a61617aea508cc1f926f2ff0b0788d5411
Alexia <alexiamiha@icloud.com>
2022-08-23 11:14:23 +0100

    added the data in the tables

--- create.py
+++ create.py
@@ -6,27 +6,31 @@ from schema import *
 # d.create_all()
 
 # Task - 2 (only once)
-# # c1 = NessCapsules(capsule_id=1,capsule_name='Oxford',price='UK',quantity='Oxana')
-# d.session.add(c1)
-# # c1 = NessCapsules(capsule_id=1,capsule_name='Oxford',price='UK',quantity='Oxana')
-# d.session.add(c1)
-# # c1 = NessCapsules(capsule_id=1,capsule_name='Oxford',price='UK',quantity='Oxana')
-# d.session.add(c1)
-# # c1 = NessCapsules(capsule_id=1,capsule_name='Oxford',price='UK',quantity='Oxana')
-# d.session.add(c1)
+# # Ness1 = NessCapsules(capsule_id=1,capsule_name='Arondio',price=0.50,quantity=0.50)
+# d.session.add(Ness1)
+# # Ness2 = NessCapsules(capsule_id=2,capsule_name='Stormio',price=0.65,quantity=150)
+# d.session.add(Ness1)
+# # Ness3 = NessCapsules(capsule_id=3,capsule_name='Intenso',price=1.50,quantity=0.50)
+# d.session.add(Ness1)
+# # Ness4 = NessCapsules(capsule_id=4,capsule_name='Scuro',price=2,50,quantity=180)
+# d.session.add(Ness1)
 
 
 # d.session.commit()
 
-# p1 = Programs(program_id=1,program_name='M.S. Data Science',fees=5000,college_id=1)
-# d.session.add(p1)
-
-
+# c1 = CustomerAccounts(customer_id=101,customer_name='Irini',paymethod=card,capsule_id=1)
+# d.session.add(c1)
+# c2 = CustomerAccounts(customer_id=102,customer_name='Alexia',paymethod=card,capsule_id=2)
+# d.session.add(c2)
+# c3 = CustomerAccounts(customer_id=103,customer_name='Dimitris',paymethod=card,capsule_id=4)
+# d.session.add(c3)
+# c4 = CustomerAccounts(customer_id=104,customer_name='Fotis',paymethod=card,capsule_id=1)
+# d.session.add(c4)
 
 d.session.commit()
 
 # Task - 3 (multiple times)
-# for clg in Colleges.query.all():
-#     print("- ",clg.college_name)
-#     for program in clg.programs:
-#         print("\t - ",program.program_name)
+# for nes in NessCapsules.query.all():
+#     print("- ",nes.capsule_name)
+#     for customeraccount in nes.customeraccounts:
+#         print("\t - ",customeraccount.customer_name)
