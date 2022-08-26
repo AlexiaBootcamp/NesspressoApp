fc5288c3bb9de9a398e72bab9cf865d0cb3c17e9
Alexia <alexiamiha@icloud.com>
2022-08-26 16:23:07 +0100

    code changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from application import application
+from application import app
 
 if __name__ == '__main__':
-    application.run(debug=True,host='0.0.0.0',port='8085')
+    app.run(debug=True,host='0.0.0.0',port='8085')
